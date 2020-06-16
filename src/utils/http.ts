23a1dfe5fa6e0f3877d588ab320e375ec7e32c59
zoneneo <zoneneo@hotmail.com>
2020-06-16 19:00:54 +0800

    create vue-cli

---
+++ src/utils/http.ts
@@ -0,0 +1,190 @@
+import axios from 'axios';
+import { Message } from 'element-ui';
+
+axios.defaults.timeout = 5000;
+axios.defaults.baseURL ='';
+
+
+//http request 拦截器
+axios.interceptors.request.use(
+  config => {
+    // const token = getCookie('token');
+    config.data = JSON.stringify(config.data);
+    config.headers = {
+      'Content-Type':'application/x-www-form-urlencoded'
+    }
+    // if(token){
+    //   config.params = {'token':token}
+    // }
+    return config;
+  },
+  error => {
+    return Promise.reject(err);
+  }
+);
+
+
+//http response 拦截器
+axios.interceptors.response.use(
+  response => {
+    if(response.data.errCode ==2){
+      router.push({
+        path:"/login",
+        query:{redirect:router.currentRoute.fullPath}
+      })
+    }
+    return response;
+  },
+  error => {
+    return Promise.reject(error)
+  }
+)
+
+
+
+axios.interceptors.response.use(    
+    response => {   
+        // 如果返回的状态码为200，说明接口请求成功，可以正常拿到数据 
+        if (response.status === 200) {            
+            return Promise.resolve(response);        
+        } else {            
+            return Promise.reject(response);        
+        }    
+    },    
+    error => {            
+        if (error.response.status) {            
+            switch (error.response.status) {                
+                // 401: 未登录
+                // 未登录则跳转登录页面，并携带当前页面的路径
+                // 在登录成功后返回当前页面，这一步需要在登录页操作。                
+                case 401:                    
+                    router.replace({                        
+                        path: '/login',                        
+                        query: { 
+                            redirect: router.currentRoute.fullPath 
+                        }
+                    });
+                    break;
+
+                // 403 token过期
+                // 登录过期对用户进行提示
+                // 清除本地token和清空vuex中token对象
+                // 跳转登录页面                
+                case 403:
+                     Toast({
+                        message: '登录过期，请重新登录',
+                        duration: 1000,
+                        forbidClick: true
+                    });
+                    // 清除token
+                    localStorage.removeItem('token');
+                    store.commit('loginSuccess', null);
+                    // 跳转登录页面，并将要浏览的页面fullPath传过去，登录成功后跳转需要访问的页面 
+                    setTimeout(() => {                        
+                        router.replace({                            
+                            path: '/login',                            
+                            query: { 
+                                redirect: router.currentRoute.fullPath 
+                            }                        
+                        });                    
+                    }, 1000);                    
+                    break; 
+
+                // 404请求不存在
+                case 404:
+                    Toast({
+                        message: '网络请求不存在',
+                        duration: 1500,
+                        forbidClick: true
+                    });
+                    break;
+                // 其他错误，直接抛出错误提示
+                default:
+                    Toast({
+                        message: error.response.data.message,
+                        duration: 1500,
+                        forbidClick: true
+                    });
+            }
+            return Promise.reject(error.response);
+        }
+    }    
+});
+
+
+/**
+ * 封装get方法
+ * @param url
+ * @param data
+ * @returns {Promise}
+ */
+
+export function fetch(url,params={}){
+  return new Promise((resolve,reject) => {
+    axios.get(url,{
+      params:params
+    })
+    .then(response => {
+      resolve(response.data);
+    })
+    .catch(err => {
+      reject(err)
+    })
+  })
+}
+
+
+/**
+ * 封装post请求
+ * @param url
+ * @param data
+ * @returns {Promise}
+ */
+
+ export function post(url,data = {}){
+   return new Promise((resolve,reject) => {
+     axios.post(url,data)
+          .then(response => {
+            resolve(response.data);
+          },err => {
+            reject(err)
+          })
+   })
+ }
+
+ /**
+ * 封装patch请求
+ * @param url
+ * @param data
+ * @returns {Promise}
+ */
+
+export function patch(url,data = {}){
+  return new Promise((resolve,reject) => {
+    axios.patch(url,data)
+         .then(response => {
+           resolve(response.data);
+         },err => {
+           reject(err)
+         })
+  })
+}
+
+ /**
+ * 封装put请求
+ * @param url
+ * @param data
+ * @returns {Promise}
+ */
+
+export function put(url,data = {}){
+  return new Promise((resolve,reject) => {
+    axios.put(url,data)
+         .then(response => {
+           resolve(response.data);
+         },err => {
+           reject(err)
+         })
+  })
+}
+
